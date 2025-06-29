3ed7a52e720baeb72d545fcc73899cf2baf288e9
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2025-06-29 17:34:04 +0300

    invitation1, invitation2, invitation3 headlines removed

--- DressUpVampires/Assets/Scripts/GameManager.cs
+++ DressUpVampires/Assets/Scripts/GameManager.cs
@@ -77,7 +77,7 @@ public class GameManager : MonoBehaviour
                     accessorySlots[i].currentTag = AccessoryTags.None;
                     accessorySlots[i].EmptySlot();
                 }
-                invitationText.text = "Invitation 1\r\nWe kindly invite you to the Seven Seas Masquarade of Lady Selene Zvuk. We ask you to the dress to the occasion.\r\nNote:\r\nWe welcome hair thats light like sea foam,\r\nclothes that remind us of fearless swashbucklers\r\nwe especially adore puffy sleaves";
+                invitationText.text = "We kindly invite you to the Seven Seas Masquarade of Lady Selene Zvuk. We ask you to the dress to the occasion.\r\nNote:\r\nWe welcome hair thats light like sea foam,\r\nclothes that remind us of fearless swashbucklers\r\nwe especially adore puffy sleaves";
                 masqueradeCounter++;
                 hint1 = false;
                 hint2 = false;
@@ -91,7 +91,7 @@ public class GameManager : MonoBehaviour
                     accessorySlots[i].currentTag = AccessoryTags.None;
                     accessorySlots[i].EmptySlot();
                 }
-                invitationText.text = "Invitation 2\r\nWith respect we invite you to the Royal Masqurade as hosted by Countess Ramona Renfield. We ask you to dress to the occasion.\r\nNote:\r\nOur countess is looking for well tailored suits that do not shy away from a great amounts of fabric, including but not limited to ruffles.";
+                invitationText.text = "With respect we invite you to the Royal Masqurade as hosted by Countess Ramona Renfield. We ask you to dress to the occasion.\r\nNote:\r\nOur countess is looking for well tailored suits that do not shy away from a great amounts of fabric, including but not limited to ruffles.";
                 masqueradeCounter++;
                 difficultyScale = 2;
                 break;
@@ -105,19 +105,19 @@ public class GameManager : MonoBehaviour
                 }
                 if(hint1 & !hint2)
                 {
-                    invitationText.text = "Invitation 3\r\nWith utmost respect we invite you to the Skin Masquerade, lovingly hosted by the feared Lord Relyn Von Valtien. We ask you to dress to the occasion.\r\nNote:\r\nA person should feel comfort in their own skin. Tonight you may show just how comfortable you are… \r\n The host enjoys…" + storeHint1;
+                    invitationText.text = "With utmost respect we invite you to the Skin Masquerade, lovingly hosted by the feared Lord Relyn Von Valtien. We ask you to dress to the occasion.\r\nNote:\r\nA person should feel comfort in their own skin. Tonight you may show just how comfortable you are… \r\n The host enjoys…" + storeHint1;
                 }
                 else if(!hint1 & hint2)
                 {
-                    invitationText.text = "Invitation 3\r\nWith utmost respect we invite you to the Skin Masquerade, lovingly hosted by the feared Lord Relyn Von Valtien. We ask you to dress to the occasion.\r\nNote:\r\nA person should feel comfort in their own skin. Tonight you may show just how comfortable you are… \r\n The host enjoys…" + storeHint2;
+                    invitationText.text = "With utmost respect we invite you to the Skin Masquerade, lovingly hosted by the feared Lord Relyn Von Valtien. We ask you to dress to the occasion.\r\nNote:\r\nA person should feel comfort in their own skin. Tonight you may show just how comfortable you are… \r\n The host enjoys…" + storeHint2;
                 }
                 else if (hint1 & hint2)
                 {
-                    invitationText.text = "Invitation 3\r\nWith utmost respect we invite you to the Skin Masquerade, lovingly hosted by the feared Lord Relyn Von Valtien. We ask you to dress to the occasion.\r\nNote:\r\nA person should feel comfort in their own skin. Tonight you may show just how comfortable you are… \r\n The host enjoys… " + storeHint1 + storeHint2;
+                    invitationText.text = "With utmost respect we invite you to the Skin Masquerade, lovingly hosted by the feared Lord Relyn Von Valtien. We ask you to dress to the occasion.\r\nNote:\r\nA person should feel comfort in their own skin. Tonight you may show just how comfortable you are… \r\n The host enjoys… " + storeHint1 + storeHint2;
                 }
                 else
                 {
-                    invitationText.text = "Invitation 3\r\nWith utmost respect we invite you to the Skin Masquerade, lovingly hosted by the feared Lord Relyn Von Valtien. We ask you to dress to the occasion.\r\nNote:\r\nA person should feel comfort in their own skin. Tonight you may show just how comfortable you are…";
+                    invitationText.text = "With utmost respect we invite you to the Skin Masquerade, lovingly hosted by the feared Lord Relyn Von Valtien. We ask you to dress to the occasion.\r\nNote:\r\nA person should feel comfort in their own skin. Tonight you may show just how comfortable you are…";
                 }
                 masqueradeCounter++;
                 difficultyScale = 3;
